34335ffb59ba12b31b99fdf5894ec45067e45435
Venky Venkatakrishnan <eeshwar1@gmail.com>
2020-01-20 21:57:10 -0600

    Initial Commit

---
+++ Sudoku/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sudoku
+//
+//  Created by Venky Venkatakrishnan on 1/20/20.
+//  Copyright © 2020 Venky UL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sudoku/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by Venky Venkatakrishnan on 1/20/20.
+//  Copyright © 2020 Venky UL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
